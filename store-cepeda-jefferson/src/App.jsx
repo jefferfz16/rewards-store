a76d212cdcfe45bd3565f25f1e15c375d430d005
jefferfz16 <jefferfz16@gmail.com>
2021-09-01 11:12:44 -0500

    eliminación de archivos por default y creación de carpetas principales

---
+++ store-cepeda-jefferson/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./assets/global-styles/style.css";
+/*components */
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <Home />
+  );
+}
+
+export default App;
---
+++ store-cepeda-jefferson/src/pages/Home.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Home() {
+    return (
+        <h1>Pagina principal</h1>
+    )
+}
